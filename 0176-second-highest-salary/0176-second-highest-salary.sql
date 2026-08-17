5ec03c95681fc29462829d0c282eb00db3c4c63b
VENKAT BABA YEMINENI <venkatbaba23@gmail.com>
2026-08-17 20:27:41 +0530

    Time: 400 ms (10.38%), Space: 0B (100%) - LeetHub

---
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+select ( select salary from Employee 
+where salary <(Select max(salary) from Employee) 
+order by salary desc limit 1) as SecondHighestSalary;
