aa50b371e6339614e0780fe70a69736427372df0
VENKAT BABA YEMINENI <venkatbaba23@gmail.com>
2026-08-24 17:36:49 +0530

    Time: 31 ms (6.35%), Space: 20.5 MB (6.82%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+
+        def dfs(n):
+            if n==1:
+                return True
+            if n==0:
+                return False
+            if n%3!=0:
+                return False
+            return dfs(n/3)
+        return dfs(n)
