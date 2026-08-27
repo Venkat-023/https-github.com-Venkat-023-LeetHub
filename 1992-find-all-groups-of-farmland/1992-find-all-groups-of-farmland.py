5c4f26fd98653c994938a8e2e0a2b79286227dda
VENKAT BABA YEMINENI <venkatbaba23@gmail.com>
2026-08-27 19:02:06 +0530

    Time: 308 ms (13.91%), Space: 62.6 MB (9.4%) - LeetHub

--- 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.py
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.py
@@ -5,10 +5,9 @@ class Solution:
         top_left=(r,c)
         bottom_right=(0,0)
         def dfs(i,j):
-            nonlocal top_left,bottom_right
+            nonlocal bottom_right
             if i<0 or j<0 or i>=r or j>=c or land[i][j]==0 or (i,j) in seen:
                 return 
-            top_left=min(top_left,(i,j))
             bottom_right=max(bottom_right,(i,j))
             seen.add((i,j))
             dfs(i+1,j)
@@ -20,9 +19,8 @@ class Solution:
         for i in range(r):
             for j in range(c):
                 if land[i][j]==1 and (i,j) not in seen:
-                    top_left=(r,c)
                     bottom_right=(-1,-1)
                     dfs(i,j)
-                    if top_left!=(r,c) and bottom_right!=(-1,-1):
-                        res.append(top_left+bottom_right)
+                    if bottom_right!=(-1,-1):
+                        res.append((i,j)+bottom_right)
         return res
